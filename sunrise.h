46b537181de0f806e67867aa2826386792cd55f3
antaldf <antaldf@gmail.com>
2016-11-21 16:45:08 -0800

    update changes

---
+++ sunrise.h
@@ -0,0 +1,38 @@
+/*
+Copyright (c) 2016 Dave Antal
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+/*
+ * File:   sunrise.h
+ * Author: antaldf
+ *
+ * Created on November 9, 2016, 8:29 AM
+ */
+
+
+#define RED_PIN  17
+#define GREEN_PIN  22
+#define BLUE_PIN  24
+
+#define RED_MAX 255
+#define GREEN_MAX  180
+#define BLUE_MAX  180
+
+
